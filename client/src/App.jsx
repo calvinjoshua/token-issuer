4dfc7c93ca6d068fda9b6d3fd39b684e7981177f
Calvin <calvin@diamanteblockchain>
2024-07-08 15:15:10 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,216 @@
+import { useState } from "react";
+import "./App.css";
+
+import {
+  Keypair,
+  Horizon,
+  Networks,
+  TransactionBuilder,
+  BASE_FEE,
+  Operation,
+  Asset,
+} from "diamante-sdk-js";
+
+function App() {
+  const [issuer, setIssuer] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [myAddress, setMyAddress] = useState("");
+  const [assetName, setAssetName] = useState("");
+  const [asset, setAsset] = useState("");
+
+  const handleMakeIssuer = async () => {
+    try {
+      let headersList = {
+        Accept: "*/*",
+      };
+
+      let response = await fetch("http://localhost:3000/create_issuer", {
+        method: "GET",
+        headers: headersList,
+      });
+
+      let data = await response.json();
+      setIssuer(data.issuer);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleFundIssuer = async () => {
+    const ext_resp = await window.diam.connect();
+    if (ext_resp.status === 200) {
+      setMyAddress(ext_resp.message[0]);
+
+      const server = new Horizon.Server("https://diamtestnet.diamcircle.io");
+      const sourceAccount = await server.loadAccount(ext_resp.message[0]);
+
+      const transaction = new TransactionBuilder(sourceAccount, {
+        fee: BASE_FEE,
+        networkPassphrase: "Diamante Testnet",
+      })
+        .addOperation(
+          Operation.createAccount({
+            destination: issuer,
+            startingBalance: "10",
+          })
+        )
+        .setTimeout(0)
+        .build();
+
+      const xdr = transaction.toXDR("base64");
+
+      const resp = await window.diam.sign(xdr, true, "Diamante Testnet");
+
+
+
+      if (resp.response.status === 200) {
+        alert("Issuer account active");
+      } else {
+        alert("Something went wrong!");
+      }
+
+      console.log(resp.toString(), "Checkkkkk");
+    } else {
+      alert("Error");
+    }
+  };
+
+  const handleMintbtnClick = async () => {
+
+    console.log("we have got it")
+    let headersList = {
+      Accept: "*/*",
+      "Content-Type": "application/json",
+    };
+
+    let bodyContent = JSON.stringify({
+      asset_name: assetName,
+    });
+
+    let response = await fetch("http://localhost:3000/create_asset", {
+      method: "POST",
+      body: bodyContent,
+      headers: headersList,
+    });
+
+    let data = await response.json();
+
+
+    console.log(data, " data received")
+
+    if (response.status !== 200) {
+      alert("IA generation failed");
+
+
+    }
+
+    setAsset(assetName);
+
+
+    const asset = new Asset(data.data.asset_name, data.data.issuer_address);
+
+    const ext_resp = await window.diam.connect();
+    const server = new Horizon.Server("https://diamtestnet.diamcircle.io");
+    const sourceAccount = await server.loadAccount(ext_resp.message[0]);
+
+    var transaction = new TransactionBuilder(sourceAccount, {
+      fee: BASE_FEE,
+      networkPassphrase: "Diamante Testnet",
+    })
+      .addOperation(
+        Operation.payment({
+          destination: "GCH2IR3YYTLTON2AUV3NW56HTFJBDAPGIZIFDSHG4FQQ5COUVG3K2PGC",
+          asset: Asset.native(),
+          amount: "4",
+        })
+      )
+      .addOperation(Operation.changeTrust({ asset }))
+
+      .setTimeout(0)
+      .build();
+
+    var xdr = transaction.toXDR("base64");
+
+    var resp = await window.diam.sign(xdr, true, "Diamante Testnet");
+    if (resp.response.status !== 200) {
+      alert("fee payment failed");
+    }
+
+
+    // if (response.status === 200) {
+
+    if (resp.response.status === 200) {
+      alert("Fee payment and trusline creation done");
+    }
+    // const server = new Horizon.Server("https://diamtestnet.diamcircle.io");
+
+
+    // const receiverAddress = await server.loadAccount(myAddress);
+
+    // const transaction = new TransactionBuilder(receiverAddress, {
+    //   fee: BASE_FEE,
+    //   networkPassphrase: "Diamante Testnet",
+    // })
+    //   .addOperation(Operation.changeTrust({ asset }))
+    //   .setTimeout(0)
+    //   .build();
+
+    // const xdr = transaction.toXDR("base64");
+    // const resp = await window.diam.sign(xdr, true, "Diamante Testnet");
+    // if (resp.response.status === 200) {
+    //   alert("Trustline created for asset ", data.data.asset_name);
+    // }
+    // }
+  };
+
+  const handleTransferAsset = async () => {
+    let headersList = {
+      Accept: "*/*",
+      "Content-Type": "application/json",
+    };
+
+    let bodyContent = JSON.stringify({
+      address: myAddress,
+      asset_name: asset,
+    });
+
+    console.log(asset)
+
+    let response = await fetch("http://localhost:3000/mint_asset", {
+      method: "POST",
+      body: bodyContent,
+      headers: headersList,
+    });
+
+    let data = await response.text();
+    console.log(data);
+  };
+
+  return (
+    <>
+      <button onClick={handleMakeIssuer}>Make Issuer</button>
+      {issuer && <div>Issuer is {issuer}</div>}
+
+      <button onClick={handleFundIssuer}>Fund Issuer</button>
+
+      <form>
+        <input
+          type="text"
+          value={assetName}
+          onChange={(e) => {
+            setAssetName(e.target.value);
+          }}
+        />
+        <button type="button" onClick={handleMintbtnClick}>
+          create Asset
+        </button>
+      </form>
+
+      <button type="button" onClick={handleTransferAsset}>
+        mint asset
+      </button>
+    </>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,164 @@
+const express = require("express");
+const diamSdk = require("diamante-sdk-js");
+const fs = require("fs");
+const cors = require("cors");
+const { error } = require("console");
+
+var intermediary_keypair;
+
+// const intermediary_keypair = diamSdk.Keypair.fromSecret("SDVQ742V2GZZBAIKUKG7DET55KQOMS633A4FXE67Q2C7TZEI4IQWQBRH");
+
+
+const app = express();
+app.use(express.json());
+
+app.use(cors());
+
+app.get("/create_issuer", (req, res) => {
+  const keypair = diamSdk.Keypair.random();
+
+  const keys_object = {
+    public_key: keypair.publicKey(),
+    private_key: keypair.secret(),
+  };
+
+  const keys = JSON.stringify(keys_object);
+
+  fs.writeFile("keypair.json", keys, "utf8", (err) => {
+    if (err) {
+      console.log("Error while writing to the file:", err);
+    } else {
+      console.log("Successfully wrote to the file.");
+    }
+  });
+
+  res.send({
+    issuer: keypair.publicKey(),
+  });
+});
+
+app.post("/create_asset", (req, res) => {
+  const asset_name = req.body.asset_name;
+
+  intermediary_keypair = diamSdk.Keypair.random();
+
+  let keys;
+  fs.readFile("keypair.json", "utf8", async (err, data) => {
+    if (err) {
+      res.send({
+        error: "Error reading file",
+        data: null,
+      });
+    } else {
+      keys = JSON.parse(data);
+      try {
+        const server = new diamSdk.Horizon.Server(
+          "https://diamtestnet.diamcircle.io"
+        );
+        const sourceAccount = await server.loadAccount(keys.public_key);
+        const sourceKeypair = diamSdk.Keypair.fromSecret(keys.private_key);
+
+        const transaction = new diamSdk.TransactionBuilder(sourceAccount, {
+          fee: diamSdk.BASE_FEE,
+          networkPassphrase: "Diamante Testnet",
+        })
+          .addOperation(
+            diamSdk.Operation.createAccount({
+              destination: intermediary_keypair.publicKey(),
+              startingBalance: "3",
+            })
+          )
+          .setTimeout(0)
+          .build();
+
+        transaction.sign(sourceKeypair);
+        const resp = await server.submitTransaction(transaction);
+        if (resp.successful == true) {
+          console.log("Succesfull!!");
+          res.send({
+            error: null,
+            data: {
+              asset_name: asset_name,
+              issuer_address: intermediary_keypair.publicKey(),
+            },
+          });
+        } else {
+          res.send({
+            error: "Transaction failed",
+            data: null,
+          });
+        }
+      } catch (e) {
+        res.send({
+          error: e,
+          data: null,
+        });
+      }
+    }
+  });
+});
+
+app.post("/mint_asset", async (req, res) => {
+  const receiver_addr = req.body.address;
+  const asset_name = req.body.asset_name;
+
+
+  console.log(receiver_addr + " 11111111111", intermediary_keypair.publicKey())
+
+  try {
+    const server = new diamSdk.Horizon.Server(
+      "https://diamtestnet.diamcircle.io"
+    );
+
+    const account = await server.loadAccount(intermediary_keypair.publicKey());
+    const _asset = new diamSdk.Asset(
+      asset_name,
+      intermediary_keypair.publicKey()
+    );
+
+    const transaction = new diamSdk.TransactionBuilder(account, {
+      fee: diamSdk.BASE_FEE,
+      networkPassphrase: "Diamante Testnet",
+    })
+      .addOperation(
+        diamSdk.Operation.payment({
+          destination: receiver_addr,
+          asset: _asset,//diamSdk.Asset.native(),
+          amount: "0.0000001",
+        })
+      )
+      // .addOperation
+      // (
+      //   diamSdk.Operation.manageData({
+      //     name: asset_name,
+      //     value: "QmPK1s3pNYLi9ERiq3BDxKa4XosgWwFRQUydHUtz4YgpqB"
+      //   })
+      // )
+      // .addOperation(
+      //   diamSdk.Operation.setOptions({
+      //     masterWeight: 0,
+      //   })
+      // )
+      .setTimeout(0)
+      .build();
+
+    transaction.sign(intermediary_keypair);
+    const result = await server.submitTransaction(transaction);
+    if (result.successful === true) {
+      res.send({
+        error: null,
+        data: "Asset transfered",
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    res.send({
+      error: e,
+      data: null,
+    });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
